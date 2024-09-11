17a3a84659cc7984c524a6ce82572b39dca4973c
Loic Messy <loiccarrel.messy@gmail.com>
2024-09-11 16:29:26 +0200

    implementation kafka producer

---
+++ src/main/java/com/komastech/kafka_producer_ex/KafkaProducerExApplication.java
@@ -0,0 +1,13 @@
+package com.komastech.kafka_producer_ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProducerExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaProducerExApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/komastech/kafka_producer_ex/KafkaProducerExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.komastech.kafka_producer_ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProducerExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
